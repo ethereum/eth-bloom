425dc48ea4a4bc728799a4484c90af6ebcddd376
Anirudha Bose <ani07nov@gmail.com>
2018-02-08 22:29:45 +0100

    Do not specify any default backend to be installed with eth-hash

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     py_modules=['eth_bloom'],
     setup_requires=['setuptools-markdown'],
     install_requires=[
-        "eth-hash[pycryptodome]>=0.1.*",
+        "eth-hash>=0.1.0a3,<0.2.0",
     ],
     license="MIT",
     zip_safe=False,
