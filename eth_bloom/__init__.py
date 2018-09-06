306c40bab219e8ebbdc0e33bf605e1237c5975e3
Dylan Wilson <dylanjw@protonmail.com>
2018-09-06 13:08:11 -0700

    Remove support for python 3.4

--- eth_bloom/__init__.py
+++ eth_bloom/__init__.py
@@ -4,9 +4,5 @@ import warnings
 from .bloom import BloomFilter  # noqa: F401
 
 
-if sys.version_info.major < 3:
-    warnings.simplefilter('always', DeprecationWarning)
-    warnings.warn(DeprecationWarning(
-        "The `eth-bloom` library is dropping support for Python 2.  Upgrade to Python 3."
-    ))
-    warnings.resetwarnings()
+if sys.version_info < (3, 5):
+    raise EnvironmentError("Python 3.5 or above is requires")
