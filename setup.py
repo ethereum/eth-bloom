7a7a5a8fbc47cebd37fa6bb8d06ac7343f2b970c
Nick Gheorghita <nickgheorghita@gmail.com>
2021-04-21 16:17:02 +0100

    Bump version: 1.0.3 → 1.0.3-0.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ extras_require['dev'] = (
 setup(
     name='eth-bloom',
     # *IMPORTANT*: Don't manually change the version here. Use the 'bumpversion' utility.
-    version='1.0.3',
+    version='1.0.3-0.1',
     description="""Python implementation of the Ethereum Trie structure""",
     long_description_markdown_filename='README.md',
     author='Piper Merriam',
